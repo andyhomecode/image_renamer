7cd13a8c1902510c33bf5c248068647ee699291b
Andy <andy@maxwell.nyc>
2025-04-04 15:30:58 -0400

    edit the batch

--- photos/rename_batch.bat
+++ photos/rename_batch.bat
@@ -1,3 +1,2 @@
 mkdir "photos\deleted"
-rename "photos\2025 01 03 birthday pants label.JPG" "2024 02 11 lulu.jpg"
-rename "photos\IMG_7776.JPG" "2024 02 25 Kudamatsu.jpg"
+rename "photos\2025 01 03 birthday pants label.JPG" "2025 01 03 birthday pants label.jpg"
